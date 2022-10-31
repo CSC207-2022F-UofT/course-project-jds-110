72acca693a0c64b9980b21ab78b177ca5c9066a5
Diian-r <dianrong2012@gmail.com>
2022-10-30 20:58:14 -0400

    Created new Leaderboard and LeaderboardPlace Classes. Implemented updateLeaderboard in the former, and implemented a getter for the money and updater for farmNames.
    *still need to add an updateLeaderboard call in getLeaderboard with the curr stats

---
+++ src/main/java/LeaderboardUseCase/Leaderboard.java
@@ -0,0 +1,30 @@
+package LeaderboardUseCase;
+
+import java.util.ArrayList;
+
+public class Leaderboard {
+    public static ArrayList<LeaderboardPlace> standings;
+//    ex. [a, b] where a and b are classes of LeaderBoardPlace
+//    a has money of 200 and name of 'MyFarm' and b has money of 100 and name of 'YourFarm'
+
+    public ArrayList<LeaderboardPlace>getLeaderboard(){
+        // add a call to updateLeaderboard with proper attributes
+        return standings;
+    }
+
+    public void updateLeaderboard(int loadedMoney, String loadedFarmName){
+        // assuming that load farm will then call this with the farm's money
+        for (int i = 0; i < standings.size(); i++){
+            LeaderboardPlace currFarm = standings.get(i);
+            if (loadedMoney == currFarm.getAmountOfMoney()) {
+                currFarm.updateFarmNames(loadedFarmName);
+            }
+            if (loadedMoney > currFarm.getAmountOfMoney()) {
+                // insert loadedMoney here so the rest get pushed back
+                LeaderboardPlace newPlace = new LeaderboardPlace(loadedMoney, loadedFarmName);
+                standings.add(i, newPlace);
+            }
+        }
+    }
+
+}
