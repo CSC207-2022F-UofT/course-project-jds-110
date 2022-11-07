311c30c404299bdaa1d895ca7710f37089607015
Diian-r <dianrong2012@gmail.com>
2022-11-07 15:01:49 -0500

    Created LeaderboardController class (needs to be implemented).
    Implemented getLeaderboard method. May need to change the call to get the name of the farm later.

--- src/main/java/LeaderboardUseCase/Leaderboard.java
+++ src/main/java/LeaderboardUseCase/Leaderboard.java
@@ -1,5 +1,7 @@
 package LeaderboardUseCase;
 
+import Use_Cases.InventoryManager;
+
 import java.util.ArrayList;
 
 public class Leaderboard {
@@ -7,12 +9,14 @@ public class Leaderboard {
 //    ex. [a, b] where a and b are classes of LeaderBoardPlace
 //    a has money of 200 and name of 'MyFarm' and b has money of 100 and name of 'YourFarm'
 
-    public ArrayList<LeaderboardPlace>getLeaderboard(){
-        // add a call to updateLeaderboard with proper attributes
+    public static ArrayList<LeaderboardPlace> getLeaderboard(){
+        int currMoney = InventoryManager.getMyInventoryMoney();
+        String name = state.getname(); // **make sure call to state is correct
+        updateLeaderboard(currMoney, name);
         return standings;
     }
 
-    public void updateLeaderboard(int loadedMoney, String loadedFarmName){
+    public static void updateLeaderboard(int loadedMoney, String loadedFarmName){
         // assuming that load farm will then call this with the farm's money
         for (int i = 0; i < standings.size(); i++){
             LeaderboardPlace currFarm = standings.get(i);
