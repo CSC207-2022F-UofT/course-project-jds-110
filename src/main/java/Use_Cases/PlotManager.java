4488b7e2ce1df8bba61d9bccc9ed2fdb58dd5520
angelaaajing <ajing.yu@mail.utoronto.ca>
2022-11-07 15:26:13 -0500

    -Add setters and getters for Inventory Manager and PlotManager -Add rent attribute to InventoryManager -Add increaseRent and removeRent method to InventoryManager and add a call of increaseRent in PlotManager.createNewPlot.

--- src/main/java/Use_Cases/PlotManager.java
+++ src/main/java/Use_Cases/PlotManager.java
@@ -1,4 +1,5 @@
 package Use_Cases;
+import Entities.Inventory;
 import Entities.Plot;
 
 import java.lang.reflect.Array;
@@ -7,17 +8,16 @@ import java.util.ArrayList;
 public class PlotManager {
     private static ArrayList<Plot> myPlots = new ArrayList<>();
 
+    public static void setMyPlots(ArrayList<Plot> arr) { myPlots = arr; }
+
     public static ArrayList<Plot> getMyPlots() {
         return myPlots;
-    }
-
-    public static void addPlot(){
-        // TO DO
     }
 
     public static void createNewPlot(){
         int newPlotId = myPlots.size() - 1;
         myPlots.add(new Plot(newPlotId));
+        InventoryManager.increaseRent();
         // create a new plot instance and add it to myPLOTS
     }
 }
