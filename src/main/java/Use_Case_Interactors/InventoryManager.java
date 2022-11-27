714cc7caf4e0d55b6120174080d24e5bcc513edb
arya <sid19arya@gmail.com>
2022-11-27 13:23:03 -0500

    Converted Inventory to a string:integer hashmap to circumvent bug that didn't allow us to use Product classes as key values. - Added ProductStringDictionary so that Inventory manager methods still have the same input and output classes and implementation for all use cases does not need to be changed - this dictinary is used to convert the 'string' key values into products when needed as products

--- src/main/java/Use_Case_Interactors/InventoryManager.java
+++ src/main/java/Use_Case_Interactors/InventoryManager.java
@@ -1,18 +1,21 @@
 package Use_Case_Interactors;
-import Entities.BakedPotato;
-import Entities.Inventory;
-import Entities.Product;
+import Entities.*;
 
 import java.util.HashMap;
 
 public class InventoryManager {
 
-    private static HashMap<String, Product>productStringDictionary = new HashMap<>();
+    private static final HashMap<String, Product>productStringDictionary = new HashMap<>();
 
     private static Inventory myInventory;
 
     public static HashMap<Product, Integer> getMyInventoryItems() {
-        return myInventory.getMyItems();
+        HashMap<Product, Integer>productHashmap = new HashMap<>();
+        for (String i : myInventory.getMyItems().keySet()){
+            productHashmap.put(productStringDictionary.get(i), myInventory.getMyItems().get(i));
+        }
+        return productHashmap;
+        // return myInventory.getMyItems();
     }
 
 
@@ -33,23 +36,24 @@ public class InventoryManager {
     }
 
     public static boolean checkIfAvailable(Product item, int amount){
-        return myInventory.getMyItems().get(item) >= amount;
+
+        return myInventory.getMyItems().get(item.getName()) >= amount;
     }
 
 
     public static void addItem(Product item, int amount) {
-        if (myInventory.getMyItems().containsKey(item)){
-            myInventory.addAnotherItem(item, amount);
+        if (myInventory.getMyItems().containsKey(item.getName())){
+            myInventory.addAnotherItem(item.getName(), amount);
         }
         else {
-            myInventory.addItem(item, amount);
+            myInventory.addItem(item.getName(), amount);
         }
     }
 
     public static Boolean removeItem(Product item, int amount) {
-        if (myInventory.getMyItems().containsKey(item)){
-            if (myInventory.getMyItems().get(item) > 1){
-                myInventory.removeSome(item, amount);
+        if (myInventory.getMyItems().containsKey(item.getName())){
+            if (myInventory.getMyItems().get(item.getName()) > 1){
+                myInventory.removeSome(item.getName(), amount);
             } else {
                 myInventory.removeItem(item);
             }
@@ -69,7 +73,18 @@ public class InventoryManager {
 
     public static void setupProductStringDictionary(){
         // add BakedPotato, Chicken, Corn, CornCob, COw, Egg, Milk, Potato, RipeTomato, Sheep, Tomato, Wool
-        productStringDictionary.put("BakedPotato", (Product) new BakedPotato());
+        productStringDictionary.put("BakedPotato",new BakedPotato());
+        productStringDictionary.put("Chicken",new Chicken());
+        productStringDictionary.put("Corn",new Corn());
+        productStringDictionary.put("CornCob",new CornCob());
+        productStringDictionary.put("Cow",new Cow());
+        productStringDictionary.put("Egg",new Egg());
+        productStringDictionary.put("Milk",new Milk());
+        productStringDictionary.put("Potato",new Potato());
+        productStringDictionary.put("Sheep",new Sheep());
+        productStringDictionary.put("Tomato",new Tomato());
+        productStringDictionary.put("Wool",new Wool());
+
         /// stilltodo
     }
 }
