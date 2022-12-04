631e6217ddbd5495f1d47e57342f9abca1952664
Diian-r <dianrong2012@gmail.com>
2022-12-03 19:40:01 -0500

    Started LeaderboardScreen. Changed it so Leaderboard Controller returns leaderboard.

--- src/main/java/LeaderboardUseCase/LeaderboardController.java
+++ src/main/java/LeaderboardUseCase/LeaderboardController.java
@@ -1,34 +1,11 @@
 package LeaderboardUseCase;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class LeaderboardController {
 
-    /**
-     * This function converts the leaderboard into a nice string with emojis.
-     * @return The rankings in a human friendly way with emojis.
-     * Example: 🥇 Number1 with $1111
-     *          🥈 Number2 with $1100
-     *          🥉 Player with $500
-     */
-    public static StringBuilder getRanking(){
-        ArrayList<LeaderboardPlace> currStandings = Leaderboard.getLeaderboard();
+    public static ArrayList<LeaderboardPlace> whatIsTheLeaderboard() {
 
-        StringBuilder returnString = new StringBuilder();
-
-        HashMap<String, String> medals = new HashMap<String, String>();
-        medals.put("0", "\uD83E\uDD47");
-        medals.put("1", "\n \uD83E\uDD48");
-        medals.put("2", "\n \uD83E\uDD49");
-
-        for (LeaderboardPlace place : currStandings) {
-            String index = String.valueOf(currStandings.indexOf(place));
-            String currString = medals.get(index) + " " + place.getFarmNamesString()
-                    + " with $" + place.getAmountOfMoney();
-            returnString.append(currString);
-        }
-
-        return returnString;
+        return Leaderboard.getLeaderboard();
     }
 }
