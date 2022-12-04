38635a234db680236cf4a133cbe1375facfcdcf0
Diian-r <dianrong2012@gmail.com>
2022-12-03 21:43:11 -0500

    Somewhat working LeaderboardScreen

--- src/main/java/UI/E_LeaderboardScreen.java
+++ src/main/java/UI/E_LeaderboardScreen.java
@@ -1,40 +1,30 @@
 package UI;
 
+import LeaderboardUseCase.LeaderboardController;
+import game_UI.A_MainDisplay;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class E_LeaderboardScreen {
-    String name;
+public class E_LeaderboardScreen extends A_MainDisplay {
     //window
     JFrame window;
     Container con;
     //title name
     JPanel titlePanel;
     JLabel titleLabel;
-
-    //backButton
-    JPanel backButtonPanel;
-    JButton backButton;
-
-    //leaderboardScreen
-    JPanel  LeaderboardTextPanel, LeaderboardPanel;
-    JLabel  LeaderboardTextLabel;
-    LeaderboardtoMainMenuHandler lmHandler = new LeaderboardtoMainMenuHandler();
-    JTextField jtf;
-    JButton enterB;
-
-
-
+    //startButton
+    JPanel startButtonPanel;
+    JButton startButton;
     //fonts
-    Font titleFont = new Font("Times New Roman", Font.PLAIN, 96);
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 50);
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 25);
+    //transition from Title Screen to Sign in Screen
+    //A_TitleScreen.TitleScreenHandler tsHandler = new A_TitleScreen.TitleScreenHandler();
 
-    public static void main(String[] args){
-        new E_LeaderboardScreen();
-    }
     public E_LeaderboardScreen(){
+        //have to put startButton here for order of layers
+        startButton = new JButton("Back");
 
         //makes window
         window = new JFrame();
@@ -49,18 +39,51 @@ public class E_LeaderboardScreen {
         titlePanel = new JPanel();
         titlePanel.setBounds(100,100,600,150);
         titlePanel.setBackground(Color.black);
-        titleLabel = new JLabel("Leaderboard", SwingConstants.CENTER);
+//
+//        StringBuilder leaderboard = LeaderboardController.getRanking();
+//
+//        if (leaderboard.toString().length() == 0) {
+//            leaderboard = new StringBuilder("No Leaderboard");
+//        }
+//
+//        titleLabel = new JLabel(leaderboard.toString(), SwingConstants.CENTER);
+        titleLabel = new JLabel("\uD83E\uDD47 Number1 with $1111\n" +
+                "\uD83E\uDD48 Number2 with $1100\n" +
+                "\uD83E\uDD49 Player with $500", SwingConstants.CENTER);
         titleLabel.setForeground(Color.white);
-        titleLabel.setFont(titleFont);
+        titleLabel.setFont(normalFont);
+
+        //makes startButton
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300,400,200,100);
+        startButtonPanel.setBackground(Color.black);
+
+        startButton.setBackground(Color.blue);
+        startButton.setForeground(Color.black);
+        startButton.setFont(normalFont);
+
+        titlePanel.add(titleLabel);
+        startButtonPanel.add(startButton);
+
+        con.add(titlePanel);
+        con.add(startButtonPanel);
 
     }
 
-    //from leaderboard screen back to main menu screen
-    public class LeaderboardtoMainMenuHandler implements ActionListener{
-        public void actionPerformed( ActionEvent event){
-            name = jtf.getText();
-            new E_MainMenuScreen(); // **??**
-        }
+    private static void createAndShowGUI() {
+        //Create and set up the content pane.
+        E_LeaderboardScreen newContentPane = new E_LeaderboardScreen();
+//        newContentPane.setOpaque(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 
 
