f81700e8413f1cfbe7fb5cd9199f9ad538718027
DavidGorospe <david.gorospe@mail.utoronto.ca>
2022-12-04 00:25:51 -0500

    Edited UI to connect each usecase screen
    - UI can transition between mainscreen and checkprogress screen

--- src/main/java/UI/E_CheckProgressScreen.java
+++ src/main/java/UI/E_CheckProgressScreen.java
@@ -1,50 +1,64 @@
 package UI;
 
 import CheckProgressUseCase.CheckProgressController;
+import Entities.Egg;
+import Entities.Inventory;
+import Entities.Plot;
+import Entities.Product;
+import Use_Case_Interactors.InventoryManager;
+import Use_Case_Interactors.PlotManager;
 
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 
 public class E_CheckProgressScreen extends A0_MainScreen{
-    public static void createAndShowGUI() {
-
-        //Create and set up the window.
-        JFrame frame = new JFrame("Check Your Progress!");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+    public E_CheckProgressScreen(){
+        //Display the window.
+        window.pack();
+        window.setVisible(true);
+        window.setSize(800,600);
+        window.getContentPane().setBackground(Color.black);
+        window.setLayout(new FlowLayout());
+        window.setTitle("Check Your Progress!");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Make Buttons
         //Display progress
         JButton btn_check_progress = new JButton("Display Progress");
         btn_check_progress.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                Product egg = new Egg();
+                Inventory i = new Inventory();
+                InventoryManager.setMyInventory(i);
+                InventoryManager.setupProductStringDictionary();
+                InventoryManager.addItem(egg, 5);
+                ArrayList<Plot> p = new ArrayList<>();
+                PlotManager.setMyPlots(p);
+                PlotManager.createNewPlot();
                 String info = CheckProgressController.gameProgress();
-                JOptionPane.showMessageDialog(frame, info);
+                JOptionPane.showMessageDialog(window, info);
             }
         });
-        frame.add(btn_check_progress);
+        window.add(btn_check_progress);
 
         //Go to main menu
         JButton btn_main_menu = new JButton("Go to Main Menu");
         btn_main_menu.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+               A0_MainScreen.createAndShowMainMenuScreen();
+               window.setVisible(false);
             }
         });
-        frame.add(btn_main_menu);
-
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-        frame.setSize(800,600);
-        frame.getContentPane().setBackground(Color.black);
-        frame.setLayout(new FlowLayout());
-
+        window.add(btn_main_menu);
+    }
+    public static void createAndShowGUI() {
+        E_CheckProgressScreen a = new E_CheckProgressScreen();
     }
     public static void main (String[]args){
         createAndShowGUI();
