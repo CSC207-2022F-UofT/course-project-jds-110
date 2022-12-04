58ae05a553c99f00082917065c7e6bc63f4d2d90
arya <sid19arya@gmail.com>
2022-12-03 19:32:16 -0500

    Changed naming conventions as before, within the sharer getFarmAppearance method.

--- src/main/java/share_use_case/Sharer.java
+++ src/main/java/share_use_case/Sharer.java
@@ -44,12 +44,12 @@ public class Sharer {
 
     public static StringBuilder getFarmAppearance() {
         HashMap<String, String> emojis = new HashMap<String, String>();
-        emojis.put("Chicken(s)", "\uD83D\uDC13");
-        emojis.put("Corn", "\uD83C\uDF3D");
-        emojis.put("Cow(s)", "\uD83D\uDC04");
-        emojis.put("Potato(es)", "\uD83E\uDD54");
-        emojis.put("Sheep", "\uD83D\uDC11");
-        emojis.put("Tomato(es)", "\uD83C\uDF45");
+        emojis.put("chicken", "\uD83D\uDC13");
+        emojis.put("corn", "\uD83C\uDF3D");
+        emojis.put("cow", "\uD83D\uDC04");
+        emojis.put("potato", "\uD83E\uDD54");
+        emojis.put("sheep", "\uD83D\uDC11");
+        emojis.put("tomato", "\uD83C\uDF45");
         // The Hashmap only contains Animals and Crops since
         // those are the only Products that can be planted on the Plots.
 
